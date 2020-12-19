17afaee0dc7a196c3e2b59886fd87cd5fd5c0951
SABBAR ElMEhdi <sabbar.mehdi94@gmail.com>
2020-12-19 22:29:28 +0000

    Largest prime factor for a given number

---
+++ ProjectEuler/LargestPrimeFactor_Problem3.java
@@ -0,0 +1,22 @@
+package ProjectEuler;
+
+import java.util.Scanner;
+
+public class LargestPrimeFactor_Problem3{
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            long n = in.nextLong();
+            int prime = 2 ;
+            for(int i=2; i<= n; i++){
+                    while(n%i ==0){
+                        n /=i;
+                    }
+                prime = i;
+            }
+            System.out.println(prime);
+        }
+    }
+}
