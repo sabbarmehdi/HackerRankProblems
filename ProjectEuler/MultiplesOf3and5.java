e14a6977816c7d2c91a5b1d16e5fc97f967c9812
SABBAR ElMEhdi <sabbar.mehdi94@gmail.com>
2020-12-19 22:34:08 +0000

    Multiples Of 3 and 5

---
+++ ProjectEuler/MultiplesOf3and5.java
@@ -0,0 +1,21 @@
+package ProjectEuler;
+
+import java.util.Scanner;
+
+public class MultiplesOf3and5 {
+    
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int sum=0;
+            for(int i=0; i<n; i++){
+                if(i%3==0 || i%5==0) {
+                    sum += i;}}
+            System.out.println(sum);
+        }
+    }
+    
+}
